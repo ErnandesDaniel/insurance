2f02aef6a5511c628bc8cd190077cbc37f6dfcc2
LeonidAlekseev <leonidalekseevv@mail.ru>
2024-11-16 01:14:41 +0300

    Add top header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,21 +2,23 @@ import type { Metadata } from "next";
 import "../styles/reset.css";
 import "../styles/global.css";
 
-import React from 'react';
-import { AntdRegistry } from '@ant-design/nextjs-registry';
+import React from "react";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+import TopHeader from "@/components/TopHeader/TopHeader";
 import Header from "@/components/Header/Header";
 
 export const metadata: Metadata = {
   title: "Виртуальный ассистент",
 };
 
-export default function RootLayout({ children }: React.PropsWithChildren){
+export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <html lang="ru">
-    <body>
-      <Header />
-      <AntdRegistry>{children}</AntdRegistry>
-    </body>
+      <body>
+        <TopHeader />
+        <Header />
+        <AntdRegistry>{children}</AntdRegistry>
+      </body>
     </html>
   );
 }
---
+++ src/components/TopHeader/TopHeader.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+import Text from "@/components/Universal/Text/Text";
+import clsx from "clsx";
+
+import Flex from "antd/es/flex";
+
+export default function TopHeader() {
+  return (
+    <div className="topHeader">
+      <div className="content">
+        <Flex className="brandBlock" gap={20}>
+          <Text>Лого</Text>
+          <Text>Страхование это легко</Text>
+        </Flex>
+      </div>
+    </div>
+  );
+}
