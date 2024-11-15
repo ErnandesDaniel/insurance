e810b3e2474c17123e8d2c0bf3d7a1d42141d95c
LeonidAlekseev <leonidalekseevv@mail.ru>
2024-11-16 01:33:35 +0300

    Add page padding

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,13 +1,13 @@
-import type {ReactNode} from "react";
+import type { ReactNode } from "react";
+import clsx from "clsx";
+import "./Page.css";
 
 interface PageProps {
-    children: ReactNode;
-    className?:string;
+  children: ReactNode;
+  className?: string;
 }
-const Page=({children, className}:PageProps)=>{
-    return(<div className={className}>
-        {children}
-    </div>)
-}
+const Page = ({ children, className }: PageProps) => {
+  return <div className={clsx("page", className)}>{children}</div>;
+};
 
 export default Page;
