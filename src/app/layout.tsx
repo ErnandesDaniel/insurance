4b3fda067cfea273bae867f0db12c13d62778446
LeonidAlekseev <leonidalekseevv@mail.ru>
2024-11-17 01:32:47 +0300

    Set base font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import React from "react";
 import clsx from "clsx";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 
-import { FontSans, FontSerif, FontMono } from "@/components/Base/Font/Font";
 import TopHeader from "@/components/TopHeader/TopHeader";
 import Header from "@/components/Header/Header";
 
@@ -18,7 +17,10 @@ export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <html
       lang="ru"
-      className={clsx(FontSans.variable, FontSerif.variable, FontMono.variable)}
+      style={{
+        fontFamily:
+          "-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans',sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol','Noto Color Emoji'",
+      }}
     >
       <body>
         <TopHeader />
