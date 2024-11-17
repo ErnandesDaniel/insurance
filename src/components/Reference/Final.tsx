23af94ae688311192f90abec8eb2ea9bff6d210b
LeonidAlekseev <leonidalekseevv@mail.ru>
2024-11-17 08:22:35 +0300

    Add final table

--- src/components/Reference/Final.tsx
+++ src/components/Reference/Final.tsx
@@ -5,7 +5,7 @@ import TextField from "@/components/Universal/TextField/TextField";
 import Form from "antd/es/form";
 import Select from "@/components/Universal/Select/Select";
 import Button from "@/components/Universal/Button/Button";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback, useMemo } from "react";
 import {
   DatePicker,
   InputNumber,
@@ -14,6 +14,7 @@ import {
   Row,
   Col,
   Popover,
+  Table,
 } from "antd";
 import ConditionalRender from "@/components/Universal/ConditionalRender/ConditionalRender";
 import Flex from "antd/es/flex";
@@ -25,19 +26,220 @@ import {
 import Input from "antd/es/input";
 import AntButton from "antd/es/button";
 import { QuestionCircleOutlined } from "@ant-design/icons";
+import type { ColumnsType } from "antd/es/table";
+
+interface ParamItem {
+  text: string;
+  action: "plus" | "mul";
+  param: number;
+}
+
+interface CategoryValues {
+  [key: string]: ParamItem;
+}
+
+interface TableRow {
+  [key: string]: string | number;
+}
+
+interface ParamsDict {
+  [key: string]: {
+    [key: string]: number;
+  };
+}
 
 export default function FinalReferences({
+  key = 1,
   form,
   referencesList,
   selectedReferences,
 }) {
   const [finalReferences, setFinalReferences] = useState<any>([]);
+  const [finalData, setFinalData] = useState<any>({
+    name: "Новый параметр",
+    baseParam: 1.0,
+    data: {},
+  });
 
   const formFields: any = Form.useWatch([], form);
-  // useEffect(() => {
-  //   console.log("formFields", formFields);
-  //   console.log("finalReferences", finalReferences);
-  // }, [formFields, finalReferences]);
+  useEffect(() => {
+    console.log("finalData", finalData);
+  }, [finalData]);
+
+  useEffect(() => {
+    const newData = { ...finalData };
+
+    referencesList
+      ?.filter(({ id }) => finalReferences.includes(id))
+      .forEach(({ name }) => {
+        if (formFields && formFields[name]) {
+          newData.data[name] = {};
+          formFields[name].forEach((option, index) => {
+            let value;
+            if (option.name) {
+              value = option.name;
+            } else if (option.from || option.to) {
+              value = "";
+              if (option.from) {
+                value += `от ${option.from} `;
+              }
+              if (option.to) {
+                value += `до ${option.to}`;
+              }
+            } else if (option.range.length > 0) {
+              value = "";
+              if (option.range[0]) {
+                value += `от ${option.range[0].format("DD.MM.YYYY")} `;
+              }
+              if (option.range[1]) {
+                value += `до ${option.range[1].format("DD.MM.YYYY")}`;
+              }
+            } else {
+              value = "Не определено";
+            }
+
+            newData.data[name][index] = {
+              text: value,
+              action: finalData.data[name][index]?.action ?? "mul",
+              param: finalData.data[name][index]?.param ?? 1.0,
+            };
+          });
+        }
+      });
+
+    referencesList
+      ?.filter(({ id }) => !finalReferences.includes(id))
+      .forEach(({ name }) => {
+        delete newData.data[name];
+      });
+
+    setFinalData(newData);
+  }, [finalReferences, formFields, referencesList]);
+
+  const handleNameChange = (value) => {
+    setFinalData((prev) => ({
+      ...prev,
+      name: value,
+    }));
+  };
+
+  const handleBaseParamChange = (value) => {
+    setFinalData((prev) => ({
+      ...prev,
+      baseParam: value,
+    }));
+  };
+
+  const handleParamChange = (name, index, value) => {
+    setFinalData((prev) => ({
+      ...prev,
+      data: {
+        ...prev.data,
+        [name]: {
+          ...prev.data[name],
+          [index]: {
+            ...prev.data[name][index],
+            param: value,
+          },
+        },
+      },
+    }));
+  };
+
+  const handleActionChange = (name, index, value) => {
+    setFinalData((prev) => ({
+      ...prev,
+      data: {
+        ...prev.data,
+        [name]: {
+          ...prev.data[name],
+          [index]: {
+            ...prev.data[name][index],
+            action: value,
+          },
+        },
+      },
+    }));
+  };
+
+  const [combinations, setCombinations] = useState<any[]>([]);
+  const [columns, setColumns] = useState<string[]>([]);
+  const [showTable, setShowTable] = useState(false);
+
+  const createTable = () => {
+    const { name, baseParam, data: categoryData } = finalData;
+
+    // Создаем расширенный словарь с параметрами и действиями
+    const paramsDict: {
+      [key: string]: {
+        [key: string]: { param: number; action: "plus" | "mul" };
+      };
+    } = {};
+
+    Object.entries(categoryData).forEach(
+      ([category, values]: [string, CategoryValues]) => {
+        paramsDict[category] = Object.values(values).reduce((acc, curr) => {
+          acc[curr.text] = {
+            param: curr.param,
+            action: curr.action,
+          };
+          return acc;
+        }, {} as { [key: string]: { param: number; action: "plus" | "mul" } });
+      }
+    );
+
+    // Функция для генерации всех комбинаций
+    const generateCombinations = (
+      categories: string[],
+      current: TableRow = {},
+      index: number = 0,
+      result: number = baseParam
+    ): TableRow[] => {
+      if (index === categories.length) {
+        const returnResult = { ...current };
+        returnResult[name] = result;
+        return [returnResult];
+      }
+
+      const category = categories[index];
+      const values = paramsDict[category];
+      const combinations: TableRow[] = [];
+
+      Object.entries(values).forEach(([text, { param, action }]) => {
+        const newCurrent = { ...current, [category]: text };
+        // Применяем соответствующую операцию в зависимости от action
+        const newResult = action === "mul" ? result * param : result + param;
+        combinations.push(
+          ...generateCombinations(categories, newCurrent, index + 1, newResult)
+        );
+      });
+
+      return combinations;
+    };
+
+    const categories = Object.keys(categoryData);
+    const allCombinations = generateCombinations(categories);
+
+    // Сортировка комбинаций
+    const sortedCombinations = allCombinations.sort((a, b) => {
+      for (const category of categories) {
+        if (a[category] < b[category]) return -1;
+        if (a[category] > b[category]) return 1;
+      }
+      return 0;
+    });
+
+    setColumns([...categories, name]);
+    setCombinations(sortedCombinations);
+    setShowTable(true);
+  };
+
+  const antColumns: ColumnsType = columns.map((column) => ({
+    title: column,
+    dataIndex: column,
+    key: column,
+    render: (text) => (typeof text === "number" ? text.toLocaleString() : text),
+  }));
 
   return (
     <>
@@ -47,7 +249,8 @@ export default function FinalReferences({
             fontSize: 18,
             border: "transparent",
           }}
-          defaultValue={"Тариф"}
+          defaultValue={"Новый параметр"}
+          onChange={(event) => handleNameChange(event.target.value)}
         />
         <Popover content={"Введите название создаваемого параметра"}>
           <QuestionCircleOutlined />
@@ -56,7 +259,12 @@ export default function FinalReferences({
       <Spacer space={10} />
       <Form.Item label="Базовое значение" layout="horizontal">
         <Space>
-          <InputNumber required defaultValue={1} step={0.1} />
+          <InputNumber
+            required
+            defaultValue={1.0}
+            step={0.1}
+            onChange={(value) => handleBaseParamChange(value)}
+          />
           <Popover content={"Значение параметра до применения корректировок"}>
             <QuestionCircleOutlined />
           </Popover>
@@ -87,33 +295,9 @@ export default function FinalReferences({
                 <Col key={index} span={8}>
                   <h4>{name}</h4>
                   <Spacer space={10} />
-
                   {formFields &&
                     formFields[name] &&
-                    formFields[name].map(({ name, from, to, range }, index) => {
-                      let value;
-                      if (name) {
-                        value = name;
-                      } else if (from || to) {
-                        value = "";
-                        if (range[0]) {
-                          value += `от ${from} `;
-                        }
-                        if (range[1]) {
-                          value += `до ${to}`;
-                        }
-                      } else if (range.length > 0) {
-                        value = "";
-                        if (range[0]) {
-                          value += `от ${range[0].format("DD.MM.YYYY")} `;
-                        }
-                        if (range[1]) {
-                          value += `до ${range[1].format("DD.MM.YYYY")}`;
-                        }
-                      } else {
-                        value = "Не определено";
-                      }
-
+                    formFields[name].map((option, index) => {
                       return (
                         <Row
                           key={index}
@@ -123,7 +307,14 @@ export default function FinalReferences({
                         >
                           <Col span="1">{index + 1}.</Col>
                           <Col flex="auto">
-                            <Input value={value} readOnly />
+                            <Input
+                              value={
+                                finalData.data[name]
+                                  ? finalData.data[name][index]?.text
+                                  : "Не определено"
+                              }
+                              readOnly
+                            />
                           </Col>
                           <Col flex="44px">
                             <Switch
@@ -131,14 +322,24 @@ export default function FinalReferences({
                               unCheckedChildren={<PlusOutlined />}
                               defaultChecked
                               style={{ background: "#4096ff" }}
+                              onChange={(value) =>
+                                handleActionChange(
+                                  name,
+                                  index,
+                                  value ? "mul" : "plus"
+                                )
+                              }
                             />
                           </Col>
                           <Col flex="88px">
                             <InputNumber
                               required
-                              defaultValue={1}
+                              defaultValue={1.0}
                               step={0.1}
                               style={{ width: 88 }}
+                              onChange={(value) =>
+                                handleParamChange(name, index, value)
+                              }
                             />
                           </Col>
                           <Col flex="22px">
@@ -165,7 +366,23 @@ export default function FinalReferences({
       <Spacer space={20} />
 
       {finalReferences.length > 0 && (
-        <Button title="Создать таблицу" htmlType="submit" />
+        <Button title="Создать таблицу" onClick={() => createTable()} />
+      )}
+      {showTable && (
+        <>
+          <Spacer space={20} />
+          <Table
+            columns={antColumns}
+            dataSource={combinations.map((item, index) => ({
+              ...item,
+              key: index,
+            }))}
+            pagination={false}
+            bordered
+            size="middle"
+            scroll={{ x: true }}
+          />
+        </>
       )}
     </>
   );
